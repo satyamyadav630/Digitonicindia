7f2bca5728abc7dd4e1e98d8b38d27e17d3d6498
Satyam Yadav <satyamyadav630@gmail.com>
2025-08-07 01:30:44 +0530

    hh

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -7,12 +7,8 @@ function AboutUs() {
       hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
       dignissimos odit consequuntur amet itaque nulla, fugiat optio unde quasi
       minus delectus in officia repudiandae aperiam. Culpa error voluptatibus
-      eius cumque eaque quos ipsa. Odit laudantium quos tempore, rerum quisquam
-
-
-      
-      ipsum.
-      satyam yadav
+      eius cumque eaque quos ipsa. Odit laudantium quos tempore, rerum quisqua
+      hgkjhgkjgbhlk
 
       
     </div>
