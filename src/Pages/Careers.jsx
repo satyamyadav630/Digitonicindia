5952b88a5267a3a144d16d8ce4bf0a839aee11de
Satyam Yadav <satyamyadav630@gmail.com>
2025-08-07 01:24:29 +0530

    update

--- src/Pages/Careers.jsx
+++ src/Pages/Careers.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Careers() {
   return (
-    <div>Careers</div>
+    <div>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam, ratione ea eligendi voluptas ullam totam quibusdam cupiditate maiores fugit amet temporibus beatae dolores inventore? Impedit voluptate corrupti nesciunt inventore officia.
+    </div>
   )
 }
 
